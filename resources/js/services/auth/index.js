5c8bd334bc4bf020308587928ec9a0f5a01aa164
kind-soul07 <ziekind07.com>
2020-11-12 19:09:49 +0800

    some changes

--- resources/js/services/auth/index.js
+++ resources/js/services/auth/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import ROUTER from '../../router'
 export default {
-    url: 'http://localhost:8000/',
+    url: 'https://driptea.herokuapp.com/#/',
     currentPath: false,
     productName: '',
     productPrice: null,
